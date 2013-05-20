53fd6fdb82e30808bef1da2aa9ade231de8da886
dziudek <dziudek@gmail.com>
2013-05-20 09:54:31 +0200

    Added option of the allowed HTML tags during the HTML clean up

--- mod_news_pro_gk5/helpers/gk.utils.php
+++ mod_news_pro_gk5/helpers/gk.utils.php
@@ -32,8 +32,21 @@ class NSP_GK5_Utils {
 			}
 		}
 		if($config['clean_xhtml'] == 1) {
+			$allowed_html = '';
+			
+			if(strlen(trim($config['allowed_tags'])) > 0) {
+				$allowed_html = explode(',', $config['allowed_tags']);		
+				$allowed_len = count($allowed_html);
+				
+				for($i = 0; $i < $allowed_len; $i++) {
+					$allowed_html[$i] = '<' . $allowed_html[$i] . '>';
+				}
+				
+				$allowed_html = implode('', $allowed_html);
+			}
+			
 			if($limit_type == 'words' && $limit_value > 0){
-				$temp = explode(' ', strip_tags($text));
+				$temp = explode(' ', strip_tags($text, $allowed_html));
 			
 				if(count($temp) > $limit_value){
 					for($i=0; $i<$limit_value; $i++) $cutted[$i] = $temp[$i];
@@ -44,7 +57,7 @@ class NSP_GK5_Utils {
 				return '';
 			} else {
 				if(JString::strlen($text) > $limit_value){
-					$text = JString::substr(strip_tags($text), 0, $limit_value) . $at_end;
+					$text = JString::substr(strip_tags($text, $allowed_html), 0, $limit_value) . $at_end;
 				}
 			}
 		} else {
