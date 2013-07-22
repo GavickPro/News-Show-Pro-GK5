f30cc8154de858ddcc35b59b21cb44cd717264f0
dziudek <dziudek@gmail.com>
2013-07-22 14:07:49 +0200

    Updated the timezone code.

--- mod_news_pro_gk5/data_sources/com_k2/model.php
+++ mod_news_pro_gk5/data_sources/com_k2/model.php
@@ -140,7 +140,9 @@ class NSP_GK5_com_k2_Model {
 		if($config['news_unauthorized'] == '0') {
 			$access_con = ' AND content.access IN ('. implode(',', JFactory::getUser()->authorisedLevels()) .') ';
 		}
-		$date = JFactory::getDate("now", $config['time_offset']);
+		$app = JFactory::getApplication();
+		$timezone = $app->getCfg('offset') + $config['time_offset'];
+		$date = JFactory::getDate("now", $timezone);
 		$now  = $date->toSql(true);
 		$nullDate = $db->getNullDate();
 		// if some data are available
