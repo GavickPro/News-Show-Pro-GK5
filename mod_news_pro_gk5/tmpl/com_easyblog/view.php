cc77a78f266129344892452a2cb43705bf597054
Tomasz Dziuda <dziudek@gmail.com>
2014-05-06 13:00:29 +0200

    Fix for the EasyBlog author avatar issues
    Ref. https://www.gavick.com/forums/game-joomla3/avatar-nsp-issues-35838.html?p=176985

--- mod_news_pro_gk5/tmpl/com_easyblog/view.php
+++ mod_news_pro_gk5/tmpl/com_easyblog/view.php
@@ -215,7 +215,14 @@ class NSP_GK5_com_easyblog_View {
 	        //
 	        $author = (trim(htmlspecialchars($item['author_alias'])) != '') ? htmlspecialchars($item['author_alias']) : htmlspecialchars($item['author_username']);
 	        $author_html = '<a href="'.urldecode(JRoute::_('index.php?option=com_easyblog&view=blogger&layout=listings&id=' . $item['author_id'])).'">'.$author.'</a>';
-	        $info_author = ($config['user_avatar'] == 1) ? '<span><img src="'.EasyBlogAvatarHelper::getAvatarUrl($item['author_id']).'" alt="'.$author.' - avatar" class="nspAvatar" width="'.$config['avatar_size'].'" height="'.$config['avatar_size'].'" /> '.$author_html.'</span>' : $author_html;
+	        
+	        // load easyblog helper
+	        require_once (JPATH_SITE.DS.'components'.DS.'com_easyblog'.DS.'helpers'.DS.'helper.php');
+ 
+		$blogger   = EasyBlogHelper::getTable( 'Profile' , 'Table' );
+		$blogger->load( $item['author_id'] );
+ 
+		$info_author = ($config['user_avatar'] == 1) ? '<span><img src="'.EasyBlogAvatarHelper::getAvatarUrl($blogger).'" alt="'.$author.' - avatar" class="nspAvatar" width="'.$config['avatar_size'].'" height="'.$config['avatar_size'].'" /> '.$author_html.'</span>' : $author_html;
 	        //
 	        $info_date = JHTML::_('date', $item['date'], $config['date_format']);
 	        //
