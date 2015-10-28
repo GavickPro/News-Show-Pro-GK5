3c42cb3e573bce837ccd2e03c7c812a397dda7a6
Tomasz Dziuda <dziudek@gmail.com>
2015-10-28 22:43:55 +0100

    New portal mode - new news header

---
+++ mod_news_pro_gk5/tmpl/portal_modes/new_news_header/controller.php
@@ -0,0 +1,143 @@
+<?php
+
+/**
+* New News Header
+* @package News Show Pro GK5
+* @Copyright (C) 2009-2015 Gavick.com
+* @ All rights reserved
+* @ Joomla! is Free Software
+* @license - http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
+* @version $Revision: GK5 1.9.2 $
+**/
+
+// access restriction
+defined('_JEXEC') or die('Restricted access');
+
+class NSP_GK5_New_News_Header {
+	// necessary class fields
+	private $parent;
+	private $mode;
+	// constructor
+	function __construct($parent) {
+		$this->parent = $parent;
+		// detect the supported Data Sources
+		if(stripos($this->parent->config['data_source'], 'com_content_') !== FALSE) {
+			$this->mode = 'com_content';
+		} else if(stripos($this->parent->config['data_source'], 'k2_') !== FALSE) { 
+			$this->mode = 'com_k2';
+		} else {
+			$this->mode = false;
+		}
+	}
+	// static function which returns amount of articles to render - VERY IMPORTANT!!
+	static function amount_of_articles($parent) {
+		if($parent->config['portal_mode_new_news_header_layout'] == 'main_2rows_2rows') {
+			return 5;
+		}
+		
+		if($parent->config['portal_mode_new_news_header_layout'] == '4cols_2cols') {
+			return 6;
+		}
+		
+		if($parent->config['portal_mode_new_news_header_layout'] == '4rows') {
+			return 4;
+		}
+		
+		return 1;
+	}
+	// output generator	
+	function output() {
+		// main wrapper
+		echo '<div class="gkNspPM gkNspPM-NewNewsHeader" id="'.$this->parent->config['module_id'].'" data-layout="'.$this->parent->config['portal_mode_new_news_header_layout'].'">';
+		
+		if(trim($this->parent->config['nsp_pre_text'])) {
+			echo $this->parent->config['nsp_pre_text'];
+		}
+		
+		// images wrapper
+		echo '<div class="gkImagesWrapper">';
+		// render images
+		for($i = 0; $i < count($this->parent->content); $i++) {			
+			if($this->get_image($i)) {
+				echo '<div class="gkImage gkImage'.($i+1).'">';
+				echo '	<div class="gkImageArea" style="background-image: url(\''.strip_tags($this->get_image($i)).'\');"></div>';
+				echo '	<h2 class="gkTitle"><a href="'.strip_tags($this->get_link($i)).'">'.strip_tags($this->parent->content[$i]['title']).'</a></h2>';
+				echo '</div>';
+			}		
+		}
+		// blank image for the proper proportions
+		echo '	<div class="gkBlankImage">';
+		echo '		<img src="data:image/png;base64,'.$this->generateBlankImage($this->parent->config['img_width'], $this->parent->config['img_height']).'" alt="" />';
+		echo '	</div>';
+		// closing images wrapper
+		echo '</div>';
+		
+		if(trim($this->parent->config['nsp_post_text'])) {
+			echo $this->parent->config['nsp_post_text'];
+		}
+		// closing main wrapper
+		echo '</div>';
+	}
+	// function used to retrieve the item URL
+	function get_link($num) {
+		if($this->mode == 'com_content') {
+			return ($this->parent->content[$num]['id'] != 0) ? JRoute::_(ContentHelperRoute::getArticleRoute($this->parent->content[$num]['id'], $this->parent->content[$num]['cid'])) : JRoute::_('index.php?option=com_users&view=login');
+		} else if($this->mode == 'com_k2') {
+			//
+			require_once (JPATH_SITE.DS.'components'.DS.'com_k2'.DS.'helpers'.DS.'route.php');
+			//
+			return urldecode(JRoute::_(K2HelperRoute::getItemRoute($this->parent->content[$num]['id'].':'.urlencode($this->parent->content[$num]['alias']), $this->parent->content[$num]['cid'].':'.urlencode($this->parent->content[$num]['cat_alias']))));
+		} else {
+			return false;
+		}
+	}
+	// image generator
+	function get_image($num) {		
+		// used variables
+		$url = false;
+		$output = '';
+		// select the proper image function
+		if($this->mode == 'com_content') {
+			// load necessary com_content View class
+			if(!class_exists('NSP_GK5_com_content_View')) {
+				require_once(JModuleHelper::getLayoutPath('mod_news_pro_gk5', 'com_content/view'));
+			}
+			// generate the com_content image URL only
+			$url = NSP_GK5_com_content_View::image($this->parent->config, $this->parent->content[$num], true, true);
+		} else if($this->mode == 'com_k2') {
+			// load necessary k2 View class
+			if(!class_exists('NSP_GK5_com_k2_View')) {
+				require_once(JModuleHelper::getLayoutPath('mod_news_pro_gk5', 'com_k2/view'));
+			}
+			// generate the K2 image URL only
+			$url = NSP_GK5_com_k2_View::image($this->parent->config, $this->parent->content[$num], true, true);
+		}
+		// check if the URL exists
+		if($url === FALSE) {
+			return false;
+		} else {
+			// if URL isn't blank - return it!
+			if($url != '') {
+				return $url;
+			} else {
+				return false;
+			}
+		}
+	}
+	// function to generate blank transparent PNG images
+	public function generateBlankImage($width, $height){ 
+		$image = imagecreatetruecolor($width, $height);
+		imagesavealpha($image, true);
+		$transparent = imagecolorallocatealpha($image, 0, 0, 0, 127);
+		imagefill($image, 0, 0, $transparent);
+		// cache the output
+		ob_start();
+		imagepng($image);
+		$img =  ob_get_contents();
+		ob_end_clean();
+		// return the string
+		return base64_encode($img);
+	}
+}
+
+// EOF
