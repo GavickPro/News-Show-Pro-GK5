542b411c7fd1006ed6141a4d1eb2905ef543dd4b
Roland Jungwirth <roland@top-node.com>
2018-08-25 12:37:47 +0200

    Pull "attribs" field from ##_content table.

--- mod_news_pro_gk5/data_sources/com_content/model.php
+++ mod_news_pro_gk5/data_sources/com_content/model.php
@@ -26,7 +26,7 @@ class NSP_GK5_com_content_Model {
 			$where2 = '';
 			$tag_join= '';
 			$tag_where = '';
-			
+
 			//
 			if($config['data_source'] == 'com_content_tags' ) {
 				$tag_join = ' LEFT JOIN #__contentitem_tag_map AS tag_map ON content.id = tag_map.content_item_id LEFT JOIN #__tags AS tag ON tag.id = tag_map.tag_id ';
@@ -41,20 +41,20 @@ class NSP_GK5_com_content_Model {
 			} else if($config['data_source'] == 'com_content_tags'){
 				$source = strpos($config['com_content_tags'],',') !== false ? explode(',', $config['com_content_tags']) : $config['com_content_tags'];
 				$where1 = ' tag.id = ';
-				$where2 = ' OR tag.id = ';	
+				$where2 = ' OR tag.id = ';
 			} else {
 				$source = strpos($config['com_content_articles'],',') !== false ? explode(',', $config['com_content_articles']) : $config['com_content_articles'];
 				$where1 = ' content.id = ';
-				$where2 = ' OR content.id = ';	
-			} 
-			//	
+				$where2 = ' OR content.id = ';
+			}
+			//
 			$where = ''; // initialize WHERE condition
 			// generating WHERE condition
 			for($i = 0;$i < count($source);$i++){
 				if(count($source) == 1) $where .= (is_array($source)) ? $where1.$source[0] : $where1.$source;
-				else $where .= ($i == 0) ? $where1.$source[$i] : $where2.$source[$i];		
+				else $where .= ($i == 0) ? $where1.$source[$i] : $where2.$source[$i];
 			}
-			
+
 			if($where != '') {
 				$where = ' AND (' . $where . ') ';
 			}
@@ -77,14 +77,14 @@ class NSP_GK5_com_content_Model {
 					$tag_where.
 					' AND 
 					c.published = 1
-		        ';	
-		        
+		        ';
+
 			// Executing SQL Query
 			$db->setQuery($query_name);
 			// check if some categories was detected
 			if($categories = $db->loadObjectList()) {
 				$categories_array = array();
-				// iterate through all items 
+				// iterate through all items
 				foreach($categories as $item) {
 					if(!in_array($item->CID, $categories_array)) {
 						array_push($categories_array, $item->CID);
@@ -100,19 +100,19 @@ class NSP_GK5_com_content_Model {
 			return null;
 		}
 	}
-	// Method to get articles in standard mode 
-	static function getArticles($categories, $config, $amount) {	
+	// Method to get articles in standard mode
+	static function getArticles($categories, $config, $amount) {
 		//
 		$sql_where = '';
 		$tag_join = '';
 		$tag_where = '';
 		$db = JFactory::getDBO();
 		//
-		if($categories) {		
+		if($categories) {
 			// getting categories ItemIDs
 			for($j = 0; $j < count($categories); $j++) {
 				$sql_where .= ($j != 0) ? ' OR content.catid = ' . $categories[$j] : ' content.catid = '. $categories[$j];
-			}	
+			}
 		}
 		// Overwrite SQL query when user set IDs manually
 		if($config['data_source'] == 'com_content_articles' && $config['com_content_articles'] != ''){
@@ -120,7 +120,7 @@ class NSP_GK5_com_content_Model {
 			$sql_where = '';
 			$ids = explode(',', $config['com_content_articles']);
 			//
-			for($i = 0; $i < count($ids); $i++ ){	
+			for($i = 0; $i < count($ids); $i++ ){
 				// linking string with content IDs
 				$sql_where .= ($i != 0) ? ' OR content.id = '.$ids[$i] : ' content.id = '.$ids[$i];
 			}
@@ -135,21 +135,21 @@ class NSP_GK5_com_content_Model {
 			//
 			$ids = explode(',', $config['com_content_tags']);
 			//
-			for($i = 0; $i < count($ids); $i++ ){	
+			for($i = 0; $i < count($ids); $i++ ){
 				// linking string with content IDs
 				$sql_where .= ($i != 0) ? ' OR tag.id = '.$ids[$i] : ' tag.id = '.$ids[$i];
 			}
-			
-			
+
+
 		}
-		
+
 		// Arrays for content
 		$content = array();
 		$news_amount = 0;
 		// Initializing standard Joomla classes and SQL necessary variables
 		$access_con = '';
-		
-		$user = JFactory::getUser();	
+
+		$user = JFactory::getUser();
 		if($config['news_unauthorized'] == '0') {
 			$access_con = ' AND content.access IN ('. implode(',', $user->getAuthorisedViewLevels()) .') ';
 		}
@@ -165,7 +165,7 @@ class NSP_GK5_com_content_Model {
 		// when showing only frontpage articles is disabled
 		$frontpage_con = '';
 		$featured_join = '';
-		
+
 		if($config['only_featured'] == 0 && $config['news_featured'] == 0) {
 		 	$frontpage_con = ' AND content.featured = 0 ';
 			$featured_join = ''; // get featured ordering
@@ -173,7 +173,7 @@ class NSP_GK5_com_content_Model {
 			$frontpage_con = ' AND content.featured = 1';
 			$featured_join = 'LEFT JOIN #__content_frontpage as content_frontpage ON id = content_id'; // get featured ordering
 		}
-		
+
 		$since_con = '';
 		if($config['news_since'] !== '') {
 			$since_con = ' AND content.created >= ' . $db->Quote($config['news_since']);
@@ -182,12 +182,12 @@ class NSP_GK5_com_content_Model {
 		if($config['news_since'] == '' && $config['news_in'] != '') {
 			$since_con = ' AND content.created >= ' . $db->Quote(strftime('%Y-%m-%d 00:00:00', time() - ($config['news_in'] * 24 * 60 * 60)));
 		}
-		
+
 		// current article hiding
 		$current_con = '';
-		
+
 		if(
-			$config['hide_current_com_content_article'] == '1' && 
+			$config['hide_current_com_content_article'] == '1' &&
 			JRequest::getCmd('option') == 'com_content' &&
 			JRequest::getCmd('view') == 'article' &&
 			JRequest::getVar('id') != ''
@@ -201,16 +201,16 @@ class NSP_GK5_com_content_Model {
 			// create the condition
 			$current_con = ' AND (content.id != '.$id.') ';
 		}
-		
+
 		// Ordering string
 		$order_options = '';
 		$rating_join = '';
 		// When sort value is random
 		if(
-					$config['news_sort_value'] == 'random'|| 
+					$config['news_sort_value'] == 'random'||
 					$config['news_sort_value'] == 'user'
 		) {
-			$order_options = ' RAND() '; 
+			$order_options = ' RAND() ';
 		} else if($config['news_sort_value'] == 'rating') {
 			$order_options = ' (content_rating.rating_sum / content_rating.rating_count) '.$config['news_sort_order'];
 			$rating_join = 'LEFT JOIN #__content_rating AS content_rating ON content_rating.content_id = content.id';
@@ -222,20 +222,20 @@ class NSP_GK5_com_content_Model {
 		if (JFactory::getApplication()->getLanguageFilter()) {
 			$lang_filter = ' AND content.language in ('.$db->quote(JFactory::getLanguage()->getTag()).','.$db->quote('*').') ';
 		}
-		
+
 		if($config['data_source'] != 'com_content_all' && $sql_where != '') {
 			$sql_where = ' AND ( ' . $sql_where . ' ) ';
 		}
-		
+
 		// one article per page - helper variables
 		$article_id_query = 'content.id AS iid';
 		$one_article_query = '';
-		
+
 		if($config['one_article_per_category']) {
 			$article_id_query = 'MAX(content.id) AS iid, content.catid AS cid';
 			$one_article_query = ' GROUP BY content.catid ';
 		}
-		// creating SQL query			
+		// creating SQL query
 		$query_news = '
 		SELECT
 			'.$article_id_query.'				
@@ -265,9 +265,9 @@ class NSP_GK5_com_content_Model {
 		// run SQL query
 		$db->setQuery($query_news);
 		// when exist some results
-		if($news = $db->loadAssocList()) {			
+		if($news = $db->loadAssocList()) {
 			// generating tables of news data
-			foreach($news as $item) {	
+			foreach($news as $item) {
 				$content[] = $item; // store item in array
 				$news_amount++;	// news amount
 			}
@@ -280,7 +280,7 @@ class NSP_GK5_com_content_Model {
 		if($second_sql_where != '') {
 			$second_sql_where = ' AND ('.$second_sql_where.')';
 		}
-		
+
 		// second SQL query to get rest of the data and avoid the DISTINCT
 		$second_query_news = '
 		SELECT
@@ -293,6 +293,7 @@ class NSP_GK5_com_content_Model {
 			content.images AS images,
 			content.featured AS frontpage,
 			content.access AS access,
+			content.attribs AS attribs,
 			content.language AS lang,
 			categories.title AS catname, 
 			users.email AS author_email,
@@ -332,13 +333,13 @@ class NSP_GK5_com_content_Model {
 		if($news2 = $db->loadAssocList()) {
 			// load URL overrides
 			$url_overrides = false;
-			
+
 			if(isset($config['url_overrides']) && $config['url_overrides'] == '1') {
 				$override_file = JPATH_SITE . '/modules/mod_news_pro_gk5/url_overrides.json';
-				
+
 				if(JFile::exists($override_file)) {
 					$override_content = file_get_contents($override_file);
-								
+
 					if($override_content && $override_content != '') {
 						$url_overrides = json_decode($override_content, true);
 					}
@@ -351,20 +352,20 @@ class NSP_GK5_com_content_Model {
 				array_push($content_iid, $item['iid']);
 			}
 			// generating tables of news data
-			foreach($news2 as $item) {						
+			foreach($news2 as $item) {
 			   	$pos = array_search($item['iid'], $content_iid);
-				
+
 				if(
-				    $url_overrides && 
+				    $url_overrides &&
 				    is_array($url_overrides) &&
-				    count($url_overrides) > 0 && 
+				    count($url_overrides) > 0 &&
 				    isset($url_overrides['com_content'])
 				) {
 					if(isset($url_overrides['com_content'][$item['iid']])) {
 						$item['overrided_url'] = $url_overrides['com_content'][$item['iid']];
 					}
 				}
-				
+
 				if( $item['lang'] == '*' ){
 					$lang = JFactory::getLanguage();
 					$item['lang'] = $lang->getTag();
@@ -374,8 +375,8 @@ class NSP_GK5_com_content_Model {
 				$access = JComponentHelper::getParams('com_content')->get('show_noauth');
 				// set the IDs to 0 if the unauthorized items are displayed
 				if($config['news_unauthorized'] == '1') {
-					if (!($access || in_array($item['access'], $authorised))) { 
-						$item['id'] = 0; 
+					if (!($access || in_array($item['access'], $authorised))) {
+						$item['id'] = 0;
 					}
 				}
 				// merge the new data to the array of items data
@@ -394,46 +395,46 @@ class NSP_GK5_com_content_Model {
 		// Reorder items if necessary
 		if(
 			$config['news_sort_value'] == 'user' &&
-			$config['data_source'] == 'com_content_articles' && 
+			$config['data_source'] == 'com_content_articles' &&
 			$config['com_content_articles'] != ''
 		) {
 			$new_content = array();
 			$ids = explode(',', $config['com_content_articles']);
 			$query_ids = array();
-			
+
 			if(count($content)) {
 				foreach($content as $key => $item) {
 					$query_ids[$item['iid']] = $key;
 				}
-				
+
 				foreach($ids as $id) {
 					if(isset($query_ids[$id])) {
 						array_push($new_content, $content[$query_ids[$id]]);
 					}
 				}
-				
+
 				$content = $new_content;
 			}
 		}
 		// the content array
-		return $content; 
+		return $content;
 	}
-	
+
 	//
 	static function getTags($content, $config) {
-		// 
+		//
 		$db = JFactory::getDBO();
 		$counters_tab = array();
-		// 
+		//
 		if(count($content) > 0) {
 			// initializing variables
 			$sql_where = '';
 			//
-			for($i = 0; $i < count($content); $i++ ) {	
+			for($i = 0; $i < count($content); $i++ ) {
 				// linking string with content IDs
 				$sql_where .= ($i != 0) ? ' OR content.id = '.$content[$i]['iid'] : ' content.id = '.$content[$i]['iid'];
 			}
-			
+
 			if($sql_where != '') {
 				$sql_where = ' AND (' . $sql_where . ') ';
 			}
@@ -462,8 +463,8 @@ class NSP_GK5_com_content_Model {
 			// when exist some results
 			if($counters = $db->loadObjectList()) {
 				// generating tables of news data
-				foreach($counters as $item) {			
-					if(isset($counters_tab[$item->id])) {			
+				foreach($counters as $item) {
+					if(isset($counters_tab[$item->id])) {
 						$counters_tab[$item->id][$item->tag] = $item->tag_id;
 					} else {
 						$counters_tab[$item->id] = array($item->tag => $item->tag_id);
@@ -472,32 +473,32 @@ class NSP_GK5_com_content_Model {
 			}
 		}
 		//
-		for($i = 0; $i < count($content); $i++ ) {	
+		for($i = 0; $i < count($content); $i++ ) {
 			if(isset($counters_tab[$content[$i]['iid']])) {
 				$content[$i]['tags'] = $counters_tab[$content[$i]['iid']];
 			}
 		}
-		
-		
-		
+
+
+
 		return $content;
 	}
 
 	// method to get comments amount
 	static function getComments($content, $config) {
-		// 
+		//
 		$db = JFactory::getDBO();
 		$counters_tab = array();
-		// 
+		//
 		if(count($content) > 0 && $config['com_content_comments_source'] != 'none') {
 			// initializing variables
 			$sql_where = '';
 			//
-			for($i = 0; $i < count($content); $i++ ) {	
+			for($i = 0; $i < count($content); $i++ ) {
 				// linking string with content IDs
 				$sql_where .= ($i != 0) ? ' OR content.id = '.$content[$i]['iid'] : ' content.id = '.$content[$i]['iid'];
 			}
-			
+
 			if($sql_where != '') {
 				$sql_where = ' AND (' . $sql_where . ') ';
 			}
@@ -546,18 +547,18 @@ class NSP_GK5_com_content_Model {
 			// when exist some results
 			if($counters = $db->loadObjectList()) {
 				// generating tables of news data
-				foreach($counters as $item) {						
+				foreach($counters as $item) {
 					$counters_tab[$item->id] = $item->count;
 				}
 			}
 		}
 		//
-		for($i = 0; $i < count($content); $i++ ) {	
+		for($i = 0; $i < count($content); $i++ ) {
 			if(isset($counters_tab[$content[$i]['iid']])) {
 				$content[$i]['comments'] = $counters_tab[$content[$i]['iid']];
 			}
 		}
-		
+
 		return $content;
 	}
 
